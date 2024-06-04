1d26ba63fbcdab8ff360d0a745beeee8caeeab09
bottlewaterM <130052274+bottlewaterM@users.noreply.github.com>
2024-06-04 10:15:29 +0900

    Add files via upload

---
+++ Codes/fnd_test1.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/ioctl.h>
+#include<sys/stat.h>
+#include<string.h>
+#include<time.h>
+
+#define fnd "/dev/fnd"
+
+unsigned char fnd_num[4] = {0,};
+int fnds;
+unsigned char Time_Table[]={~0x3f, ~0x06, ~0x5b, ~0x4f, ~0x66, ~0x6d, ~0x7d, ~0x07, ~0x7f, ~0x67, ~0x00};
+
+int main(){
+    fnds = open(fnd, O_RDWR);
+    if(fnds < 0){printf("Can't open FND.\n"); exit(0);}
+
+    int minutes = 0;
+    int seconds = 0;
+
+    while (minutes < 100) {
+        fnd_num[0] = Time_Table[minutes / 10];
+        fnd_num[1] = Time_Table[minutes % 10];
+        fnd_num[2] = Time_Table[seconds / 10];
+        fnd_num[3] = Time_Table[seconds % 10];
+
+        write(fnds, fnd_num, sizeof(fnd_num));
+
+        sleep(1);
+
+        seconds++;
+
+        if (seconds == 60) {
+            seconds = 0;
+            minutes++;
+        }
+    }
+
+    close(fnds);
+    return 0;
+}
